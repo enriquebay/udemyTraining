01cf0659631b1eece889c2a62a82aa52f48bdb85
enriquebay <bay@yabit.io>
2017-02-08 22:34:43 -0600

    remove past

---
+++ 00_GolangTrainingMirror/12_if_else-if_else/06_if-elseif-else/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	if false {
+		fmt.Println("first print statemtn")
+	} else if true {
+		fmt.Println("second print statement")
+	} else {
+		fmt.Println("third print statement")
+	}
+
+}
